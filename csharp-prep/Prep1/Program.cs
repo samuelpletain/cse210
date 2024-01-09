c65f8ab1ac71b475aafde5dd88723202118e4b88
Samuel Pletain <samuel.pletain@gmail.com>
2024-01-08 21:48:24 -0700

    Prep 1 display fix

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,12 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("What is your first name?");
+        Console.Write("What is your first name? ");
         string firstName = Console.ReadLine();
         
-        Console.WriteLine("What is your last name?");
+        Console.Write("What is your last name? ");
         string lastName = Console.ReadLine();
 
-        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
+        Console.WriteLine($"\nYour name is {lastName}, {firstName} {lastName}.");
     }
 }
