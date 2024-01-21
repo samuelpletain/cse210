5e966cb5186467176eab491245176c77b0a9702c
Samuel Pletain <samuel.pletain@gmail.com>
2024-01-20 21:30:02 -0700

    Learning 3 solution

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,53 @@
+public class Fraction
+{
+  private int _top;
+  private int _bottom;
+
+  public Fraction()
+  {
+    _top = 1;
+    _bottom = 1;
+  }
+
+  public Fraction(int wholeNumber)
+  {
+    _top = wholeNumber;
+    _bottom = 1;
+  }
+  
+  public Fraction(int top, int bottom)
+  {
+    _top = top;
+    _bottom = bottom;
+  }
+
+  public int getTop()
+  {
+    return _top;
+  }
+
+  public int getBottom()
+  {
+    return _bottom;
+  }
+
+  public void setTop(int top)
+  {
+    _top = top;
+  }
+
+  public void setBottom(int bottom)
+  {
+    _bottom = bottom;
+  }
+
+  public string GetFractionString()
+  {
+    return _top + "/" + _bottom;
+  }
+
+  public double GetDecimalValue()
+  {
+    return (double)_top / _bottom;
+  }
+}
