7e6f8832c1d78102b5712690fd8b57a17bcfbf74
Samuel Pletain <samuel.pletain@gmail.com>
2024-01-27 15:15:28 -0700

    Removing getters and setters

--- prove/Develop03/Word.cs
+++ prove/Develop03/Word.cs
@@ -3,26 +3,6 @@ public class Word
   private string _text;
   private bool _isHidden;
 
-  public string GetText()
-  {
-    return _text;
-  }
-
-  public bool GetIsHidden()
-  {
-    return _isHidden;
-  }
-
-  public void SetText(string text)
-  {
-    _text = text;
-  }
-
-  public void SetIsHidden(bool isHidden)
-  {
-    _isHidden = isHidden;
-  }
-
   public Word(string text)
   {
     _text = text;
