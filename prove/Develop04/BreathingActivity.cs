ba06afb686a1d013f4498b3da2c373b9caec1e3f
Samuel Pletain <samuel.pletain@gmail.com>
2024-01-27 22:33:01 -0700

    Breathing activity class

--- prove/Develop04/BreathingActivity.cs
+++ prove/Develop04/BreathingActivity.cs
@@ -1,11 +1,27 @@
 public class BreathingActivity : Activity
 {
-  public BreathingActivity(string name, string description, string duration) : base(name, description, duration)
+  public BreathingActivity()
   {
+    SetName("Breathing");
+    SetDescription("relax by walking yo through breathing in and out slowly. Clear your mind and focus on your breathing.");
   }
 
   public void Run()
   {
+    DisplayStartingMessage();
     
+    DateTime startTime = DateTime.Now;
+    DateTime endTime = startTime.AddSeconds(GetDuration());
+
+    while (DateTime.Now < endTime)
+    {
+      Console.Write("Breathe in...");
+      ShowCountDown(3);
+      Console.Write("Now breathe out...");
+      ShowCountDown(3);
+      Console.WriteLine();
+    }
+
+    DisplayEndingMessage();
   }
 }
