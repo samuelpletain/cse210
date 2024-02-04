aa6b389a4dadc5d21c57a281246a6a3709763904
Samuel Pletain <samuel.pletain@gmail.com>
2024-02-03 19:42:25 -0700

    Moved lists creation in constructors

--- prove/Develop04/ListingActivity.cs
+++ prove/Develop04/ListingActivity.cs
@@ -1,18 +1,19 @@
 public class ListingActivity : Activity
 {
   private int _count = 0;
-  private List<string> _prompts = new List<string>(){
-    "Who are people that you appreciate?",
-    "What are personal strengths of yours?",
-    "Who are people that you have helped this week?",
-    "When have you felt the Holy Ghost this month?",
-    "Who are some of your personal heroes?"
-  };
+  private List<string> _prompts;
 
   public ListingActivity()
   {
     SetName("Listing");
     SetDescription("This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.");
+    _prompts = new List<string>(){
+      "Who are people that you appreciate?",
+      "What are personal strengths of yours?",
+      "Who are people that you have helped this week?",
+      "When have you felt the Holy Ghost this month?",
+      "Who are some of your personal heroes?"
+    };
   }
 
   public void Run()
