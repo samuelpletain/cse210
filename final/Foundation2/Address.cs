8b3f0a9b7b5814e12b09ebb0129cb0872cdf0d92
Samuel Pletain <samuel.pletain@gmail.com>
2024-02-10 23:05:24 -0700

    Formatting

--- final/Foundation2/Address.cs
+++ final/Foundation2/Address.cs
@@ -4,6 +4,7 @@ public class Address
   private string _city;
   private string _state;
   private string _country;
+
   public Address(string street, string city, string state, string country)
   {
     _street = street;
